5761a0ab27d5161912c6dfd6a6a039b4cbf893af
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-04-01 18:04:30 +0100

    html 1.0

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { storageAvailable } from "./app/webStorage";
+
+function createCard() {
+  title = document.querySelector("#title").value;
+}
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "/src/index.js",
+    /* home: "/src/app/home.js",
+    menu: "/src/app/menu.js",
+    location: "/src/app/location.js",
+    header: "/src/app/header.js", */
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html", //maybe "index.[contenthash].html"?
+      template: "./src/template.html",
+      hash: true,
+      title: "TOP-Todo-List",
+      meta: {
+        author: "Daniel Freire",
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader", //2. Inject styles into DOM
+          "css-loader", //1. Turns css into commonjs
+          ,
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          sources: {
+            list: [
+              // All default supported tags and attributes
+              {
+                tag: "img",
+                attribute: "data-src",
+                type: "src",
+              },
+              {
+                tag: "img",
+                attribute: "data-srcset",
+                type: "srcset",
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};

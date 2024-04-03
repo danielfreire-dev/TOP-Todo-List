c7c35a3fd465d62ffb4fb391a6c47505f082d3c8
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-04-03 18:14:52 +0100

    creating projects option

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import "/src/style/style.css";
 let choreList = [];
 let finishedList = [];
 
-function Chore(title, date, priority, description) {
+function Chore(title, date, priority, description, project, completed) {
   this.title = title;
   this.date = date;
   this.priority = priority;
   this.description = description;
+  this.project = project;
+  this.completed = completed;
+}
+
+Chore.prototype.toggleComplete = function () {
+  this.completed = !this.completed;
+};
+
+function toggleComplete(index) {
+  choreList[i].toggleComplete();
+  render();
 }
 
 function render() {
@@ -17,8 +28,8 @@ function render() {
 
   for (let i = 0; i < choreList.length; i++) {
     let chore = choreList[i];
-    console.log("choreList: ");
-    console.log(choreList[i]);
+    /* console.log("choreList: "); */
+    /* console.log(choreList[i]); */
     let choreCard = document.createElement("div");
     choreCard.setAttribute("class", "chore-card");
     choreCard.innerHTML = `
@@ -33,18 +44,13 @@ function render() {
     <h4 class="date">${chore.date}</h4>
     <div class="priority">${chore.priority}</div>
     <p class="description">${chore.description}</p>
-    <input type="submit" value="Completed" />
+    <input type="checkbox" value="Completed" id='completed' />
+
   </div>
     `;
+
     unfinishedList.appendChild(choreCard);
   }
-}
-
-function choreFinished(index) {
-  finishedList.concat(choreList[index]);
-  /* starting from 'index', removes 1 element */
-  choreList.splice(index, 1);
-  render();
 }
 
 function createCard() {
@@ -54,10 +60,10 @@ function createCard() {
   let description = document.querySelector("#description").value;
   let newChore = new Chore(title, date, priority, description);
   choreList.push(newChore);
-  console.log("newChore: ");
+  /*  console.log("newChore: ");
   console.log(newChore);
   console.log("choreList: ");
-  console.log(choreList);
+  console.log(choreList); */
   render();
 }
 
@@ -67,3 +73,18 @@ newChoreBtn.addEventListener("submit", function (event) {
   createCard();
   /* console.log(choreList); */
 });
+
+let finishedBtn = document.querySelector(".chore-card");
+finishedBtn.addEventListener("change", function choreFinished(index) {
+  console.log("checkbox checked");
+  finishedList.concat(choreList[index]);
+  choreList.splice(index, 1);
+  render();
+});
+
+/* function choreFinished(index) {
+  finishedList.concat(choreList[index]);
+  /* starting from 'index', removes 1 element */
+/*choreList.splice(index, 1);
+  render();
+} */
